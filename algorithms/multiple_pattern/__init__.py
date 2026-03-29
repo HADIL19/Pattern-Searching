237a658fabee34c09bba96a23a8e1f6a40ff6e00
HADIL19 <hadylkhelif18@gmail.com>
2026-03-29 05:43:05 +0200

    Fix import errors and publish v0.1.2 to PyPI

--- algorithms/multiple_pattern/__init__.py
+++ algorithms/multiple_pattern/__init__.py
@@ -1,12 +1,12 @@
 # algorithms/multiple_pattern/__init__.py
-from .rabin_karp_multiple import rabin_karp_multiple_search
+from .rabin_karpe_pattern import rabin_karp_multiple
 from .aho_corasick import AhoCorasick
-from .wu_manber import wu_manber_search
-from .commentz_walter import commentz_walter_search
+from .wu_manber import wu_manber
+from .commentz_walter import commentz_walter
 
 __all__ = [
-    "rabin_karp_multiple_search",
+    "rabin_karp_multiple",
     "AhoCorasick",
-    "wu_manber_search",
-    "commentz_walter_search"
+    "wu_manber",
+    "commentz_walter"
 ]
